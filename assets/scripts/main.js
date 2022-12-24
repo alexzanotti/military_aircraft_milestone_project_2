4e0dab8c5343813ec70962b08c876079a6aabeae
alexzanotti <alexanderzanotti@hotmail.com>
2022-12-24 20:26:31 +0000

    darkmode javascript

---
+++ assets/scripts/main.js
@@ -0,0 +1,5 @@
+// This sets the dark mode class to toggle, imitating a fighter aircraft colour scheme
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("darkMode");
+}
