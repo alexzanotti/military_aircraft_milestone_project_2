ed8ede5446f64271165332f2b663958bf221f995
alexzanotti <alexanderzanotti@hotmail.com>
2022-12-24 20:28:56 +0000

    add image carousel to main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,3 +3,22 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("darkMode");
 }
+
+// image carousel
+const buttonSelect = document.querySelectorAll("[data-carousel-button]");
+// function for each button
+buttonSelect.forEach(button => {
+  //add event listener to next and prev button
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+    const currentSlide = slides.querySelector("[data-active]")
+    let updateIndex = [...slides.children].indexOf(currentSlide) + offset
+    if (updateIndex < 0) updateIndex = slides.children.length - 1
+    if (updateIndex >= slides.children.length) updateIndex = 0
+    slides.children[updateIndex].dataset.active = true
+    delete currentSlide.dataset.active
+  })
+})
