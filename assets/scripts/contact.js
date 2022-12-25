0e2663aa433ec47a192b6719938e0b72d7e99d27
alexzanotti <alexanderzanotti@hotmail.com>
2022-12-25 13:16:54 +0000

    function for email submission using emailjs

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -14,3 +14,26 @@
         }
     });
 })()
+
+
+//function for email submission using emailjs
+function sendEmail() {
+    // object to collect the users name, email and message
+    var content = {
+      name: document.getElementById("userName").value,
+      email: document.getElementById("userEmail").value,
+      message: document.getElementById("userMessage").value,
+    };
+    // variable for both the emailjs serviceID and templateID
+    const serviceID = "service_ujlw0id";
+    const templateID = "template_vmke7ek";
+    // send the email
+      emailjs.send(serviceID, templateID, content)
+      .then(sent=>{
+          document.getElementById("userName").value = "";
+          document.getElementById("userEmail").value = "";
+          document.getElementById("userMessage").value = "";
+      })
+    // redirect the user to the home page upon submission of contact form
+    window.location.href="index.html";
+  }
